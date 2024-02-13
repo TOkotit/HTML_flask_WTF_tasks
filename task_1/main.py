700e12e281d4f9670b416bc1c8ebffc9a9adbe3d
TOkotit <105845895+TOkotit@users.noreply.github.com>
2024-02-13 17:14:15 +0500

    Add files via upload

---
+++ task_1/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/Заготовка')
+def prepeare_to_mission():
+    planet = 'Марс'
+    return render_template('base.html', title=planet)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
