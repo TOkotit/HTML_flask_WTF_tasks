cf6ec42b03dbdbfa04d285afd5e2b7243974b372
TOkotit <105845895+TOkotit@users.noreply.github.com>
2024-02-13 18:11:47 +0500

    Add files via upload

---
+++ task_2/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index/<title>')
+def prepeare_to_mission(title):
+    return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def trainig(prof):
+    return render_template('tranings.html', title='training', prof=prof)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
