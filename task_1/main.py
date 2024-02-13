83748853d08b19bb338cb61013302591de17d3cb
TOkotit <105845895+TOkotit@users.noreply.github.com>
2024-02-13 18:01:24 +0500

    Update main.py

--- task_1/main.py
+++ task_1/main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
 @app.route('/index/<title>')
 def prepeare_to_mission(title):
     return render_template('base.html', title=title)
